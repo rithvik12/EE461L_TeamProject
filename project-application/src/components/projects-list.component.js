29c1a5ef08594980fd342211d3eb87d865cfff79
Bharath Kaimal <bharath.kaimal@gmail.com>
2021-04-07 12:59:42 -0500

    update list of current projects

--- project-application/src/components/projects-list.component.js
+++ project-application/src/components/projects-list.component.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class ProjectsList extends Component {
+    constructor(props) {
+        super(props);
+        this.deleteProject = this.deleteProject.bind(this);
+        this.state = {projects: []};
+      }
+
+      componentDidMount() {
+        axios.get('http://localhost:5000/projects/')
+         .then(response => {
+           this.setState({ projects: response.data });
+         })
+         .catch((error) => {
+            console.log(error);
+         })
+      }
   render() {
     return (
       <div>
