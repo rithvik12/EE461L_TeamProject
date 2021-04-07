aab5d23b85dd9e3a9a20bd8c5bf15764b659511a
Bharath Kaimal <bharath.kaimal@gmail.com>
2021-04-07 12:26:26 -0500

    update project.js

--- project-application/backend/routes/projects.js
+++ project-application/backend/routes/projects.js
@@ -25,4 +25,37 @@ router.route('/add').post((req, res) => {
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// returns project item given an 'id'
+router.route('/:id').get((req, res) => {
+    Project.findById(req.params.id)
+      .then(project => res.json(project))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+// deletes project item given an 'id'
+  router.route('/:id').delete((req, res) => {
+    Project.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Project deleted.'))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+// updates existing project item
+// first retrieve old project item from database based on 'id'
+// then set project property values to what is available in request body
+// project.save saves updated object into the database
+  router.route('/update/:id').post((req, res) => {
+    Project.findById(req.params.id)
+      .then(project => {
+        project.username = req.body.username;
+        project.description = req.body.description;
+        project.duration = Number(req.body.duration);
+        project.date = Date.parse(req.body.date);
+  
+        project.save()
+          .then(() => res.json('Project updated!'))
+          .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
 module.exports = router;
