763f923df2e73c402427d654649935a7a825f38c
Bharath Kaimal <bharath.kaimal@gmail.com>
2021-04-07 12:34:11 -0500

    import react-router-dom

--- project-application/src/App.js
+++ project-application/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
  
 function App() {
  return (
-   <div className="container">
-     Hello World
-   </div>
+   <Router>
+     <div className="container">
+       Hello World
+     </div>
+   </Router>
  );
 }
  
