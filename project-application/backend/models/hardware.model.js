d9eb078609b5f97f790355f1e6d5fdf0b00e5ee8
Bharath Kaimal <bharath.kaimal@gmail.com>
2021-04-07 23:16:53 -0500

    creation of model for hardware sets

---
+++ project-application/backend/models/hardware.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const hardwareSchema = new Schema({
+    availability: { type: Number},
+    capacity: { type: Number},
+}, {
+    timestamps: true,
+});
+
+const Hardware = mongoose.model('Hardware', hardwareSchema);
+
+module.exports = Hardware;
