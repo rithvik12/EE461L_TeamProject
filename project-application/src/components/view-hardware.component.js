c28e77b56e76cc9912833bbd22c2ed21fcd6b6a7
Cathy Le <lehcathy@gmail.com>
2021-05-02 18:21:35 -0500

    Revert "Revert "updated heroku app links""
    This reverts commit 23823678a5d08c796a557302148f47389eb14d9a.

--- project-application/src/components/view-hardware.component.js
+++ project-application/src/components/view-hardware.component.js
@@ -26,7 +26,7 @@ export default class ViewHardware extends Component {
     }
 
     componentDidMount() {
-        axios.get('http://quiet-lowlands-32326/hardwares/')
+        axios.get('http://dry-reaches-42443.herokuapp.com/hardwares/')
         .then(response => {
             this.setState({ hardwares: response.data });
         })
@@ -36,7 +36,7 @@ export default class ViewHardware extends Component {
     }
 
     deleteHardware(id) {
-        axios.delete('http://quiet-lowlands-32326/hardwares/'+id)
+        axios.delete('http://dry-reaches-42443.herokuapp.com/hardwares/'+id)
         .then(res => console.log(res.data));
         this.setState({
           hardwares: this.state.hardwares.filter(el => el._id !== id)
